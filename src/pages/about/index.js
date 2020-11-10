f14b3d6d6b3333f0072332576e2866dfb0e0f62a
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2020-11-10 20:38:36 +0100

    Change home page to spanish (#6)
    * add changes to home page
    
    * change base url

---
+++ src/pages/about/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+const TITLE = "About";
+const DESCRIPTION =
+  "Project OpenUBL son un conjunto de herramientas Open Source para ayudarte a crear tu propio software de Facturación Electrónica";
+
+function About() {
+  return (
+    <Layout title={TITLE} description={DESCRIPTION}>
+      <main className="container margin-vert--lg">
+        <div className="text--center margin-bottom--xl">
+          <h1>{TITLE}</h1>
+          <img alt="Logo" src="img/logo.png" style={{ height: 100 }} />
+          <h4>Project OpenUBL</h4>
+          <div className="row">
+            <div className="col col--6 col--offset-3">
+              <p>
+                Project OpenUBL es un proyecto Open Source que incluye un
+                conjunto de herramientas, librerias, microservicios asociados a
+                la Facturación Electrónica en el Perú.
+              </p>
+              <p>
+                Project OpenUBL nace porque ví que todos crean sus propios
+                softwares y no hay ninguna iniciativa de crear soluciones de
+                nivel empresarial utilizando herramientas Open Source. El
+                conocimiento debe de ser compartido y plasmado en algo concreto.
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import clsx from "clsx";
+import classnames from "classnames";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -8,24 +9,32 @@ import styles from "./styles.module.css";
 
 const features = [
   {
-    title: "XBuilder",
-    imageUrl: "img/xbuilder-logo.svg",
+    title: "Open Source",
+    imageUrl: "img/undraw_open_source_1qxw.svg",
     description: (
       <>
-        Create XML files based on the Universal Bussiness Language (UBL).
-        Supported documents: Invoices, Credit Notes, Debit Notes, Voided
-        Document, SummaryDocument, etc.
+        Todos los proyectos dentro de <i>Project OpenUBL</i> son Open Source.
       </>
     ),
   },
   {
-    title: "XSender",
-    imageUrl: "img/xsender-logo.svg",
+    title: "Coge solo lo que necesitas",
+    imageUrl: "img/undraw_Choose_bwbs.svg",
     description: (
       <>
-        Tools used for sending XML files based on UBL (Universal Bussiness
-        Language) to Superintendencia Nacional de Aduanas y de Administración
-        Tributaria (SUNAT, Perú). No SOAP knowledge required.
+        <i>Project OpenUBL</i> está diseñado para no obligarte a "todo o nada".
+        Ya sea que desees solamente crear XMLs o solamente enviar XMLs{" "}
+        <i>Project OpenUBL</i> te permite hacerlo.
+      </>
+    ),
+  },
+  {
+    title: "Híbrido",
+    imageUrl: "img/undraw_server_q2pb.svg",
+    description: (
+      <>
+        Sin importar el lenguaje de programación en el que esté hecho tu
+        software, siempre podrás integrar tu software con <i>Project OpenUBL</i>
       </>
     ),
   },
@@ -34,14 +43,14 @@ const features = [
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx("col col--6", styles.feature)}>
+    <div className={clsx("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
       )}
       <h3 className="text--center">{title}</h3>
-      <p>{description}</p>
+      <p className="text--center">{description}</p>
     </div>
   );
 }
@@ -51,12 +60,15 @@ function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout
-      title={`${siteConfig.title}`}
-      description="Open Source set of tools for managing XML files based on UBL (Universtal Bussiness Language). Currently we support Perú's specifications based Superintendencia Nacional de Aduanas y de Adminitración Tributaria (SUNAT) but we are open to support other countries requirements as well."
+      title={siteConfig.tagline}
+      description={siteConfig.customFields.description}
     >
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
+          <img alt="Logo" src="img/logo.png" style={{ height: 100 }} />
+          <h2 className="hero__title">
+            Herramientas Open Source para la Facturación Electrónica
+          </h2>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <a
@@ -68,14 +80,14 @@ function Home() {
               rel="noopener noreferrer"
               href="https://projectopenubl.zulipchat.com/join/iwvosgykfucbi2wqwfjg2rza/"
             >
-              Join the chat
+              Únete al chat
             </a>
           </div>
         </div>
       </header>
       <main>
         {features && features.length > 0 && (
-          <section className={styles.features}>
+          <section className={classnames("margin-bottom--xl", styles.features)}>
             <div className="container">
               <div className="row">
                 {features.map((props, idx) => (
@@ -85,21 +97,75 @@ function Home() {
             </div>
           </section>
         )}
-      </main>
-      <main style={{ padding: 80 }}>
+        <section className="margin-bottom--xl">
+          <div className="container text--center">
+            <div className="row">
+              <div className="col col--4 col--offset-2">
+                <div>
+                  <img
+                    className={styles.logoImage}
+                    src="img/xbuilder-logo.svg"
+                  />
+                </div>
+                <h3>XBuilder</h3>
+                <p>
+                  <Link to="docs/xbuilder">XBuilder</Link> te permite{" "}
+                  <strong>crear y firmar XMLs</strong> basados en UBL según los
+                  estándares de la{" "}
+                  <a
+                    href="http://www.sunat.gob.pe/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    SUNAT
+                  </a>
+                  . Soporte para boletas, facturas, notas de crédito, notas de
+                  débito, bajas, resúmenes diarios, etc.
+                </p>
+              </div>
+              <div className="col col--4">
+                <div>
+                  <img
+                    className={styles.logoImage}
+                    src="img/xsender-logo.svg"
+                  />
+                </div>
+                <h3>XSender</h3>
+                <p>
+                  <Link to="docs/xsender">XSender</Link> te permite{" "}
+                  <strong>enviar XMLs</strong> a la{" "}
+                  <a
+                    href="http://www.sunat.gob.pe/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    SUNAT
+                  </a>{" "}
+                  o cualquier OSE haciendo uso de sus servicios web SOAP. No
+                  necesitas saber SOAP, <a href="docs/xsender">XSender</a> lo
+                  hace todo automáticamente por ti.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
         <section>
           <div className="container text--center">
             <div>
-              <h2>Create and send XMLs based on UBL</h2>
-              <p>
-                You can use <strong>XBuilder</strong> for creating XML files and
-                the <strong>XSender</strong> to send them to the SUNAT.
-              </p>
+              <h2>
+                Crea tu propio software de Facturación Electrónica usando las
+                librerias y herramientas de Project OpenUBL
+              </h2>
             </div>
             <div className="row">
-              <div className="col col--12">
-                <div className="text--center">
-                  <img alt="Declarative" src="img/xml-example.png" />
+              <div className="col col--6">
+                <div>
+                  <img src="img/undraw_software_engineer_lvl5.svg" />
+                </div>
+              </div>
+              <div className="col col--6">
+                <div>
+                  <img src="img/xml-example.png" />
                 </div>
               </div>
             </div>
