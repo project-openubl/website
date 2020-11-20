b87e2f446ece1f3b682e8728af76bd8b73844244
Deysi Perez <58010161+medalith2590@users.noreply.github.com>
2020-11-20 14:19:01 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "Project OpenUBL",
-  tagline: "Crea XMLs basados en UBL y después envíalos a la SUNAT",
+  tagline: "Crea XMLs basados en UBL y después envíalos a la SUNAT.",
   url: "https://project-openubl.github.io",
   baseUrl: "/",
   onBrokenLinks: "throw",
